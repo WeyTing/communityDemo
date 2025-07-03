7a6b52e52d2114fa6d870bb731e086597d8e928a
wey <45823398+WeyTing@users.noreply.github.com>
2025-07-03 19:05:45 +0800

    feat: 新增 Firebase 配置及環境變數，移除 HelloWorld 組件

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,22 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBgBarVuNnHUf3WIop6xV1PZyL9SN-Pmao",
-  authDomain: "minisocial-446f6.firebaseapp.com",
-  projectId: "minisocial-446f6",
-  storageBucket: "minisocial-446f6.firebasestorage.app",
-  messagingSenderId: "980726336207",
-  appId: "1:980726336207:web:d5b621b81ce60b902445ba",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
